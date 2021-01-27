56e5ca671de75db85dbcc51abaa23af33776c630
demonmiker <demonmiker@ya.ru>
2021-01-27 18:25:24 +0300

    update

--- TVP/Program.cs
+++ TVP/Program.cs
@@ -12,7 +12,7 @@ namespace TVP
     {
         static void Main(string[] args)
         {
-            Lab3();
+            Lab5();
         }
 
 #region Lab2
@@ -97,7 +97,68 @@ namespace TVP
         
         #endregion
 
+
+        static void Lab5()
+        {
+            string program = ReadLine() + '\0';
+            StateType State = StateType.oD;
+            int i = 0;
+            char symbol;
+            try
+            {
+
+
+                while ((symbol = program[i++]) != '\0')
+                {
+                    WriteLine($"ТС: {State}");
+                    switch (State)
+                    {
+                        case StateType.oD:
+                            switch (symbol)
+                            {
+                                case 'c': State = StateType.cD; break;
+                                default: throw new Exception();
+                            }
+                            break;
+                        case StateType.cD:
+                            switch (symbol)
+                            {
+                                case 'o': State = StateType.oD; break;
+                                case 'U': State = StateType.cU; break;
+                                default: throw new Exception();
+                            }
+                            break;
+                        case StateType.cU:
+                            switch (symbol)
+                            {
+                                case 'D': State = StateType.cD; break;
+                                case 'o': State = StateType.oU; break;
+                                default: throw new Exception();
+                            }
+                            break;
+                        case StateType.oU:
+                            switch (symbol)
+                            {
+                                case 'c': State = StateType.cU; break;
+                                default: throw new Exception();
+                            }
+                            break;
+                    }
+                }
+                WriteLine($"ТС: {State}");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Недопустимый переход");
+                ReadKey();
+            }
+            ReadKey();
+        }
     }
+
+    enum StateType {oD,cD,cU,oU};
+
+
 
     
 }
